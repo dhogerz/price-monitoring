f4cbddc1a8316cfa4596043509fa9aee812e1562
yoga.pramugia01 <p@ssw0rd1>
2020-05-17 21:54:51 +0700

    update postgresql

--- src/main/java/com/fabelio/test/SpringBootWebApplication.java
+++ src/main/java/com/fabelio/test/SpringBootWebApplication.java
@@ -2,8 +2,10 @@ package com.fabelio.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.fabelio.test")
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
